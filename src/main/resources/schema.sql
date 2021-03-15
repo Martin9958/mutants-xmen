8c3a481eefa7730b3df5a4a21a2911a291710717
Andres Martin Cantor Urrego <andres.cantor@payulatam.com>
2021-03-15 00:35:43 -0500

    Real Credentials

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,2 +1,2 @@
-CREATE SCHEMA IF NOT EXISTS brotherhood_of_mutants AUTHORIZATION xmen;
--- CREATE SCHEMA IF NOT EXISTS brotherhood_of_mutants AUTHORIZATION vciigxqwisykqs;
+-- CREATE SCHEMA IF NOT EXISTS brotherhood_of_mutants AUTHORIZATION xmen;
+CREATE SCHEMA IF NOT EXISTS brotherhood_of_mutants AUTHORIZATION vciigxqwisykqs;
